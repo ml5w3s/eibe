652de7ebf1c935934f66385aa3be1ff71129c4c8
ml5w3s <marcelo.lauriano@gmail.com>
2022-12-08 09:31:35 -0300

    "Organização das pastas"

--- banco_o14/controle/inserir_usuario.php
+++ banco_o14/controle/inserir_usuario.php
@@ -1,22 +1,31 @@
 <?php
 include("conexao.php");
 $conn = conectar();
-$user = $_POST["txt_user"];
-$mail = $_POST["txt_mail"];
-$pass = md5($_POST["txt_pass"]);
-$conf = md5($_POST["txt_conf"]);
 
+$user = $_POST['txt_user'];
+$bairro = $_POST['cbx_bairro'];
+$mail = $_POST['txt_mail'];
+$pass = md5($_POST['txt_pass']);
+$conf = md5($_POST['txt_conf']);
+
+try{
 if($pass<>$conf){
-    echo "<script>alert('Senhas não conferem')</script>
-    <a href='../formulario/cad_usuario.php'>Voltar</a>";
-    //echo "Senhas não conferem"
+    echo'<script>
+        alert("Senhas não conferem, Usuário não cadastrado");
+        window.location.href = "../formulario/cad_usuario.php";
+    </script>';
 }else{
-    $conn->query("INSERT INTO usuario(usuario,email,senha)VALUES('$user','$mail','$pass')");
-    echo'
-    <script>
+    $stm = $conn->prepare("INSERT INTO usuario(nome_usuario,bairro_usuario,senha) VALUES (?,?,?)");
+    $stm->bindValue(1,$user,PDO::PARAM_STR);
+    $stm->bindValue(2,$bairro,PDO::PARAM_STR);    
+    $stm->bindValue(3,$pass,PDO::PARAM_STR);
+    $stm->execute();
+    echo'<script>
         alert("Registro salvo com sucesso!");
-        window.location.href = "../index.html";
-    </script>
-    ';
+        window.location.href = "../formulario/cad_usuario.php";
+    </script>';
+}
+}catch(PDOException $ex_){
+    echo 'Erro '.$ex_->getMenssage();
 }
 ?>
